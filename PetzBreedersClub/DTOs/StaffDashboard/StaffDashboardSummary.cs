8e4962d6fce730dd254e30390276235ac8f8fb9a
Cairath <Cairath@users.noreply.github.com>
2023-03-18 00:01:28 +0100

    Removed comments

--- PetzBreedersClub/DTOs/StaffDashboard/StaffDashboardSummary.cs
+++ PetzBreedersClub/DTOs/StaffDashboard/StaffDashboardSummary.cs
@@ -7,6 +7,4 @@ public class StaffDashboardSummary
 	[Required] public int PendingCatRegistrations { get; set; }
 	[Required] public int PendingDogRegistrations { get; set; }
 	[Required] public int PendingAffixRegistrations { get; set; }
-//	[Required] public List<ShowSummary> OpenShows { get; set; }
-//	[Required] public List<ShowSummary> MyShows { get; set; }
 }
